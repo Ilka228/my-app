a7937b7c34892dab2fd19b277ddb1849a306485c
IlyaSolonovich <ilyasolonovich20@gmail.com>
2022-08-01 08:27:01 +0200

    feat: add all functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+type AppProps = {
+};
+type AppFile = {
+  url: string;
+  name: string;
+  
 }
+type AppState = {
+  filesList: Array<AppFile>;
+  currentImgSrc: string;
+};
+
+function getFileNameFromUrl(link: any) {
+  return decodeURI(link.split('/').pop());
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor() {
+    super({})
+    this.onChange = this.onChange.bind(this);
+    this.state = {
+      filesList: [],
+      currentImgSrc: ''
+    }
+  }
+
+  onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const files = event.target.files;
+
+    if(!files) {
+      console.log('No files');
+      return;
+    };
+
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    this.setState({filesList: this.state.filesList.length === 5 ? [...this.state.filesList, {name: file.name ,url}].slice(1) : [...this.state.filesList, {name: file.name ,url}]})
+  }
+        
+  render(): React.ReactNode {
+     
+    
+    return (
+      <div className="App">
+        <div className='storage-area'>
+          {this.state.filesList.map((item, index) => 
+            <div key={index} className="img-item">
+              <button onClick={() => this.setState({currentImgSrc: item.url})}>{getFileNameFromUrl(item.name)}</button>
+            </div>
+          )}
+          <form encType="multipart/form-data" className='img-item loader-form'>
+            
+            <label htmlFor="actual-btn" className='loader'>+ Upload Your Image</label>
+            <input type="file" id="actual-btn" accept="image/*,image/jpeg"  className='input' onChange={this.onChange}/>
+          
+          </form>
+        </div>
+        <div className='preview'>
+          {this.state.currentImgSrc === '' ? null : <img src={this.state.currentImgSrc}/>}
+        </div>
+      </div>
+      )}
+}
+
+
 
 export default App;
